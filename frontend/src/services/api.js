57bd7e371facf348e23480eab7938b5d8c3a2523
lafayete macedo <lafayette.m@gmail.com>
2026-06-22 19:53:46 -0300

    feat: make API_BASE_URL dynamic to support production VPS

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://127.0.0.1:8000';
+const API_BASE_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
+  ? 'http://127.0.0.1:8000'
+  : window.location.origin;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
